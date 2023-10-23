8d717d96f239a0ae5e4acb7aed4cc75097116c0c
crohnarodev <viniciosalfa@hotmail.com>
2023-10-23 18:46:21 -0300

    feat: :sparkles: adding text content

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,27 @@
 import styles from "./page.module.css";
 import Header from "../components/Header/header";
 import "./globals.css";
-import { Container } from "@mui/material";
+import { Box, Container } from "@mui/material";
 import ImageLogo from "@/components/ImageLogo/imageLogo";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Container maxWidth='xl'>
+      <Container maxWidth="xl">
         <Header />
       </Container>
-      <ImageLogo />
+      <Box sx={{ width: "100%", display: "flex" }}>
+        <Box className={styles.textContent}>
+          <Box>
+            <h1>Oi, eu sou o</h1>
+            <span>Vini</span>
+            <h2>Desenvolvedor Full-Stack</h2>
+          </Box>
+        </Box>
+        <Box>
+          <ImageLogo />
+        </Box>
+      </Box>
     </main>
   );
 }
