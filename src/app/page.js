f3b0589372c713f4ae25656df05ec77632687dd7
crohnaro <vinicioscararine95@gmail.com>
2024-05-20 17:08:26 -0300

    feat: :lipstick: updating ui

--- src/app/page.js
+++ src/app/page.js
@@ -10,18 +10,31 @@ export default function Home() {
       <Container maxWidth="xl">
         <Header />
       </Container>
-      <Box sx={{ width: "100%", display: "flex" }}>
-        <Box className={styles.textContent}>
+        <Box sx={{ display: "flex" }}>
+          <Box className={styles.textContent}>
+            <Box>
+              <h1>OI, EU SOU O</h1>
+              <span>Vini</span>
+              <h2>DESENVOLVEDOR FULL-STACK_</h2>
+            </Box>
+            <Box>
+              <p>
+                Olá! Sou Vinicios, Professor e Software Engineer. Iniciei minha
+                trajetória na área de Infraestrutura e Redes, mas durante a
+                faculdade de Sistemas de Informação, descobri minha afinidade
+                pelo desenvolvimento. Curso atualmente Pós Graduação em
+                Engenharia de Software. Possuo conhecimento em diversas
+                tecnologias e experiência em UX/UI. Acredito firmemente que a
+                paixão aliada ao empenho são essenciais para concretizar ideias.
+                Estou entusiasmado para contribuir e realizar um trabalho de
+                qualidade em equipe. Espero podermos colaborar juntos!!
+              </p>
+            </Box>
+          </Box>
           <Box>
-            <h1>OI, EU SOU O</h1>
-            <span>Vini</span>
-            <h2>DESENVOLVEDOR FULL-STACK_</h2>
+            <ImageLogo />
           </Box>
         </Box>
-        <Box>
-          <ImageLogo />
-        </Box>
-      </Box>
     </main>
   );
 }
