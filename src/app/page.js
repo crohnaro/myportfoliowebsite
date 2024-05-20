4b0880832e96a5c767bf0d571289b2f56d3d79d1
crohnaro <vinicioscararine95@gmail.com>
2024-05-20 19:17:16 -0300

    feat: :lipstick: improving main section UI

--- src/app/page.js
+++ src/app/page.js
@@ -10,7 +10,8 @@ export default function Home() {
       <Container maxWidth="xl">
         <Header />
       </Container>
-        <Box sx={{ display: "flex" }}>
+      <Container maxWidth="xl">
+        <Box className={styles.mainContainer}>
           <Box className={styles.textContent}>
             <Box>
               <h1>OI, EU SOU O</h1>
@@ -19,15 +20,9 @@ export default function Home() {
             </Box>
             <Box>
               <p>
-                Olá! Sou Vinicios, Professor e Software Engineer. Iniciei minha
-                trajetória na área de Infraestrutura e Redes, mas durante a
-                faculdade de Sistemas de Informação, descobri minha afinidade
-                pelo desenvolvimento. Curso atualmente Pós Graduação em
-                Engenharia de Software. Possuo conhecimento em diversas
+                Olá! Sou Vinicios, Professor e Software Engineer. Possuo conhecimento em diversas
                 tecnologias e experiência em UX/UI. Acredito firmemente que a
                 paixão aliada ao empenho são essenciais para concretizar ideias.
-                Estou entusiasmado para contribuir e realizar um trabalho de
-                qualidade em equipe. Espero podermos colaborar juntos!!
               </p>
             </Box>
           </Box>
@@ -35,6 +30,7 @@ export default function Home() {
             <ImageLogo />
           </Box>
         </Box>
+      </Container>
     </main>
   );
 }
