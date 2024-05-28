ccf270d1c92a606e3e2d12f098e8969d6076faac
crohnaro <vinicioscararine95@gmail.com>
2024-05-28 14:54:30 -0300

    refactor: :recycle: abstracting the hero page

--- src/app/page.js
+++ src/app/page.js
@@ -3,40 +3,17 @@
 import styles from "./page.module.css";
 import Header from "../components/Header/header";
 import "./globals.css";
-import { Box, Container } from "@mui/material";
-import ImageLogo from "@/components/ImageLogo/imageLogo";
-import BlackholeVideo from "@/components/BlackHoleVideo/blackholevideo";
-import MainTexts from "@/components/MainTexts/maintexts";
 
-import React, { useEffect, useRef } from "react";
-import LocomotiveScroll from "locomotive-scroll";
+import HeroPage from "./pages/HeroPage";
 
 
 
 export default function Home() {
 
-  const scrollRef = useRef(null);
-
-  useEffect(() => {
-    const scroll = new LocomotiveScroll({
-      el: scrollRef.current,
-      smooth: true,
-    });
-
-    return () => {
-      if (scroll) scroll.destroy();
-    };
-  }, []);
+  
   return (
-    <main ref={scrollRef} >
-      <Container maxWidth="xl">
-        <Header />
-        <Box className={styles.mainContainer}>
-          <MainTexts />
-          <ImageLogo />
-        </Box>
-      </Container>
-      <BlackholeVideo />
+    <main>
+      <HeroPage />
     </main>
   );
 }
